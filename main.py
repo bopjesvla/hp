4ee204f4b30ba49a927f4677c720defb75da7fac
Bob <negenentwintig@hotmail.com>
2018-03-14 11:21:06 +0100

    regularization

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ def rmsle(y, y_pred):
 scorer = metrics.make_scorer(rmsle)
 
 df = pd.read_csv('train.csv')
-hood_price = df.groupby('Neighborhood')['SalePrice'].mean().reset_index()
-hood_price.columns = ['Neighborhood', 'hood_price']
-df = df.merge(hood_price, on=['Neighborhood'])
+# hood_price = df.groupby('Neighborhood')['SalePrice'].mean().reset_index()
+# hood_price.columns = ['Neighborhood', 'hood_price']
+# df = df.merge(hood_price, on=['Neighborhood'])
 
 # date as unix timestamp
 date = pd.to_datetime({'year': df['YrSold'], 'month': df['MoSold'], 'day': 1}).astype(int).rename('date')
@@ -27,16 +27,14 @@ X = df[[c for c in df.columns if c != 'SalePrice']]
 X_float = X.select_dtypes(exclude=['object']).fillna(0)
 
 y = df['SalePrice']
-date_price = pd.concat((y, X_float['date']), axis=1)
-mean_price_by_date = date_price.groupby('date').mean()
-hist = mean_price_by_date.shift(1).fillna(200000).reset_index()
-hist.columns = ['date', 'hist_price']
-print(stats.spearmanr(mean_price_by_date.values, hist['hist_price']))
-X_float = X_float.reset_index().merge(hist, on=['date']).set_index('index')
+# date_price = pd.concat((y, X_float['date']), axis=1)
+# mean_price_by_date = date_price.groupby('date').mean()
+# hist = mean_price_by_date.shift(1).fillna(200000).reset_index()
+# hist.columns = ['date', 'hist_price']
+# print(stats.spearmanr(mean_price_by_date.values, hist['hist_price']))
+# X_float = X_float.reset_index().merge(hist, on=['date']).set_index('index')
 
 # normalizing
-X_float -= X_float.mean()
-X_float /= X_float.std()
 # X_float = pd.merge(X, hist, on=['date'])
 
 X_cat = X.select_dtypes(include=['object']).fillna('None')
@@ -46,7 +44,10 @@ X_one_hot = pd.get_dummies(X_cat)
 
 # print(X_float[Xfloat.isnull().any(axis=1)])
 # print(Xstr.columns)
-X = pd.concat((X_float, X_one_hot), axis=1)
+# X = pd.concat((X_float, X_one_hot), axis=1)
+X = X_float
+X -= X.mean()
+X /= X.std()
 # print(X['hist_price'])
 y = df['SalePrice']
 
@@ -82,26 +83,29 @@ for train, test in model_selection.KFold(K).split(X, y):
     seq = [1 for b in X_train.columns]
     c_fit = 10000
 
-    for i in range(E):
-        model = linear_model.LinearRegression(normalize=False)
-        (seq,c_fit) = OnePlusOne(seq,1/n,model,X_train,y_train,c_fit)
-        model.fit(X_train[X.columns[seq]], y_train  )
-        y_pred = model.predict(X_test[X.columns[seq]])
-        y_pred = np.maximum(y_pred, 0)
-        performance = rmsle(y_test, y_pred)
+#     for i in range(E):
+#         model = linear_model.LinearRegression(normalize=False)
+#         (seq,c_fit) = OnePlusOne(seq,1/n,model,X_train,y_train,c_fit)
+#         model.fit(X_train[X.columns[seq]], y_train  )
+#         y_pred = model.predict(X_test[X.columns[seq]])
+#         y_pred = np.maximum(y_pred, 0)
+#         performance = rmsle(y_test, y_pred)
 
-        if i == 0:
-            print('Init test performance:', performance)
+#         if i == 0:
+#             print('Init test performance:', performance)
 
-#		performance = trainModel(model, X_test[[X.columns[i] for i in range(n) if seq[i]]],y_test)
-        perf_hist[i][k] = performance
-        print( 'Fold: ' + str(k) + ' Epoch: ' + str(i) + ' Train perf: ' + str(c_fit) + ' Test perf: ' + str(performance) + "%                 ", end='\r')
-    print('Fold: ' + str(k) + ' Performance: ' + str(performance) )
+# #		performance = trainModel(model, X_test[[X.columns[i] for i in range(n) if seq[i]]],y_test)
+#         perf_hist[i][k] = performance
+#         print( 'Fold: ' + str(k) + ' Epoch: ' + str(i) + ' Train perf: ' + str(c_fit) + ' Test perf: ' + str(performance) + "%                 ", end='\r')
+#     print('Fold: ' + str(k) + ' Performance: ' + str(performance) )
 
-    model = linear_model.LinearRegression(normalize=False)
-    model.fit(X_train[X.columns[seq]], y_train)
+    model = linear_model.Ridge(1000)
+    # model = linear_model.LinearRegression()
+    # model.fit(X_train[X.columns[seq]], y_train)
+    model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test[X.columns[seq]])
+    # y_pred = model.predict(X_test[X.columns[seq]])
+    y_pred = model.predict(X_test)
     y_pred = np.maximum(y_pred, 0)
     score = rmsle(y_test, y_pred)
     scores.append(score)
